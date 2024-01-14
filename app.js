b43b0af11f0aff269ec73799247e63163c644d75
MrRobotical <mcleanj901@gmail.com>
2024-01-14 12:21:55 -0500

    100 users

--- app.js
+++ app.js
@@ -1,43 +1,41 @@
-const result = document.getElementById('result')
-const filter = document.getElementById('filter')
-const listItems =  []
+const result = document.getElementById('result');
+const filter = document.getElementById('filter');
+const listItems = [];
 
-getData()
+getData();
 
-filter.addEventListener('input', (e) => filterData(e.target.value))
+filter.addEventListener('input', (e) => filterData(e.target.value));
 
 async function getData() {
-    const res = await fetch('https://randomuser.me/api?results=50')
+  const res = await fetch('https://randomuser.me/api?results=100');
 
-    const { results }= await res.json()
+  const { results } = await res.json();
 
-    //Clear results
-  result.innerHTML = ''
+  //Clear results
+  result.innerHTML = '';
 
-results.forEach(user =>{
-    const li = document.createElement('li')
+  results.forEach((user) => {
+    const li = document.createElement('li');
 
-    listItems.push(li)
+    listItems.push(li);
 
     li.innerHTML = `
     <img src="${user.picture.large}" alt="${user.name.first}">
     <div class="user-info">
     <h4>${user.name.first} ${user.name.last}</h4>
     <p>${user.location.city}, ${user.location.country}}</p>
-    `
+    `;
 
-result.appendChild(li)
-
-})
-
+    result.appendChild(li);
+  });
 }
 
 function filterData(searchTerm) {
-    listItems.forEach(item => {
-        if(item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
-            item.classList.remove('hide')
-        } else {
-            item.classList.add('hide')
-        }
-    })
+  listItems.forEach((item) => {
+    if (item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+      item.classList.remove('hide');
+    } else {
+      item.classList.add('hide');
+    }
+  });
 }
